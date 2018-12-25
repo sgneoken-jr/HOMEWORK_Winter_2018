1b6b6f0ce97452e5b14cdde9de6b79cb3b79d5d1
sgneoken-jr <valerio.sgn@libero.it>
2018-12-25 17:44:41 +0100

    Viewer

--- run.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-MAKEFILE_FOLDER = "cd src/"
-echo MAKEFILE_FOLDER
-echo "make"
-
-echo "haptic -30 20 20 12"
--- src/viewer.c
+++ src/viewer.c
@@ -9,6 +9,7 @@
 #include "myTypes.h"
 
 
+
 void *viewer(void *inputParameters){
 	#ifdef DEBUG
 	printf("Viewer thread lauched...\n");
@@ -23,10 +24,59 @@ void *viewer(void *inputParameters){
 /*	system(newWindow);*/
 
 	while (!gracefulDegradation){
-		#ifdef DEBUG
-		printf("%s\n", "I'm the viewer");
-		#endif
+
 	}
 
 	pthread_exit(NULL);
 }
+
+// C++ code from stack overflow for a progress indicator
+// https://stackoverflow.com/questions/14539867/how-to-display-a-progress-indicator-in-pure-c-c-cout-printf
+// float progress = 0.0;
+// while (progress < 1.0) {
+//     int barWidth = 70;
+//
+//     std::cout << "[";
+//     int pos = barWidth * progress;
+//     for (int i = 0; i < barWidth; ++i) {
+//         if (i < pos) std::cout << "=";
+//         else if (i == pos) std::cout << ">";
+//         else std::cout << " ";
+//     }
+//     std::cout << "] " << int(progress * 100.0) << " %\r";
+//     std::cout.flush();
+//
+//     progress += 0.16; // for demonstration only
+// }
+// std::cout << std::endl;
+
+int updatePosBar(double pos, int barLength){
+	printf("%s", "|");
+	int indicator = adaptToRange(&pos, &barLength);
+	for (int i = 0; i < barLength; ++i){
+		if (i == indicator){
+			printf("%s", "O");
+		}
+		else {
+			printf("%s", "-");
+		}
+	}
+	printf("%s %0.4lf\n", "|", pos);
+	fflush(stdout);
+
+  return indicator;
+}
+
+double adaptToRange(double *pos, int *barLength){
+  double scaledPos;
+  double realRange = MAX - MIN;
+  double scaleFactor = *barLength / realRange;
+  double offset = ABS(MIN);
+
+  scaledPos = (*pos + offset) * scaleFactor;
+  return scaledPos;
+}
+
+// This is a problem of the stdout stream being buffered. You have to flush it explicitly (implicit flushing occurs with a \n) using fflush(stdout) after the printf():
+//
+// fflush(stdout);
